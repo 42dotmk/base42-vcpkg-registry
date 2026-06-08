083931267595ab4fce90361f972408b11a2bf6bb
Vasilaki Totsili <vasilaki@totsili.com>
2026-06-08 22:42:43 +0200

    feat: add sparq port to vcpkg
    - Introduced a new port for the `sparq` library to vcpkg to facilitate
      easy integration:
      - Included `portfile.cmake` with configuration for building and
        installing `sparq` from its GitHub repository
      - Added a `vcpkg.json` for metadata, including name, version
        and description
    - Removed `.placeholder.txt` to clean up unused files

---
+++ ports/sparq/portfile.cmake
@@ -0,0 +1,21 @@
+vcpkg_from_git(
+	OUT_SOURCE_PATH SOURCE_PATH
+	URL https://github.com/42dotmk/sparq.git
+	REF 0e182e969323d4b66b5322bc753b941d1bae9e5a
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+)
+
+vcpkg_cmake_install()
+
+vcpkg_cmake_config_fixup(
+	PACKAGE_NAME sparq
+	CONFIG_PATH lib/cmake/sparq
+)
+
+vcpkg_copy_pdbs()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
