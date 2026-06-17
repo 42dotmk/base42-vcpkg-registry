bef2ef5e6ea3f742119739189827fd25724211d8
Vasilaki Totsili <vasilaki@totsili.com>
2026-06-18 01:29:01 +0200

    chore: update sparq portfile for GitHub integration
    - Switched the source retrieval method from `vcpkg_from_git` to
      `vcpkg_from_github` to improve repository integration
    - Defined the repository as `42dotmk/sparq` and added the SHA512 hash
      for the specified commit reference to ensure integrity

--- ports/sparq/portfile.cmake
+++ ports/sparq/portfile.cmake
@@ -1,7 +1,8 @@
-vcpkg_from_git(
+vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    URL https://github.com/42dotmk/sparq.git
+    REPO 42dotmk/sparq
     REF 0e182e969323d4b66b5322bc753b941d1bae9e5a
+    SHA512 0d362dbdceea5338be1ff40040123a784cdeb5688762b02a4bc94d636daccb4175c8d46ed6b3727038206969a90b0135860901702c2692f861c7a745240e9cfc
 )
 
 vcpkg_cmake_configure(
